27ade051d1c8c110b531abd1a801a2d37efd4f61
NajwanSV <najwan.joan.sv@gmail.com>
2024-11-09 12:49:34 +0700

    first Commit

---
+++ app/src/main/java/com/samsung/android/health/sdk/sample/healthdiary/activity/HealthMainActivity.kt
@@ -0,0 +1,188 @@
+
+package com.samsung.android.health.sdk.sample.healthdiary.activity
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
+import androidx.lifecycle.viewModelScope
+import com.samsung.android.health.sdk.sample.healthdiary.R
+import com.samsung.android.health.sdk.sample.healthdiary.adapters.HeartRateAdapter
+import com.samsung.android.health.sdk.sample.healthdiary.databinding.HealthMainBinding
+import com.samsung.android.health.sdk.sample.healthdiary.utils.AppConstants
+import com.samsung.android.health.sdk.sample.healthdiary.utils.formatString
+import com.samsung.android.health.sdk.sample.healthdiary.utils.showToast
+import com.samsung.android.health.sdk.sample.healthdiary.viewmodel.HealthMainViewModel
+import com.samsung.android.health.sdk.sample.healthdiary.viewmodel.HealthViewModelFactory
+import com.samsung.android.health.sdk.sample.healthdiary.viewmodel.HeartRateViewModel
+import com.samsung.android.sdk.health.data.HealthDataStore
+import com.samsung.android.sdk.health.data.permission.AccessType
+import com.samsung.android.sdk.health.data.permission.Permission
+import com.samsung.android.sdk.health.data.request.DataTypes
+import com.samsung.android.sdk.health.data.request.LocalTimeFilter
+import com.samsung.android.sdk.health.data.request.Ordering
+import kotlinx.coroutines.Runnable
+import kotlinx.coroutines.launch
+import java.time.LocalDateTime
+
+
+
+class HealthMainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var healthMainViewModel: HealthMainViewModel
+    private lateinit var heartRateViewModel: HeartRateViewModel
+    private lateinit var bpmTextView: TextView
+
+
+    private val handler = Handler(Looper.getMainLooper())
+    private lateinit var runnable: Runnable
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        healthMainViewModel = ViewModelProvider(
+            this, HealthViewModelFactory(this)
+        )[HealthMainViewModel::class.java]
+
+        heartRateViewModel = ViewModelProvider(
+            this, HealthViewModelFactory(this)
+        )[HeartRateViewModel::class.java]
+
+        /**  Initialize OnClickListener on Heart Rate, Sleep, Nutrition and Step buttons */
+        DataBindingUtil
+            .setContentView<HealthMainBinding>(this, R.layout.health_main)
+            .run {
+                cvNutrition.setOnClickListener(this@HealthMainActivity)
+                cvStep.setOnClickListener(this@HealthMainActivity)
+                cvHeartRate.setOnClickListener(this@HealthMainActivity)
+                cvSleep.setOnClickListener(this@HealthMainActivity)
+            }
+
+        /** Show toast on exception occurrence **/
+        healthMainViewModel.exceptionResponse.observe(this) { message ->
+            showToast(this, message)
+        }
+
+        bpmTextView = findViewById(R.id.BPM)
+        collectResponse()
+        observeHeartRateData()
+        startHeartRateUpdates()
+
+//        JANLUP HAPUS INI RNG
+        heartRateViewModel.generateDummyFiveMinutesHeartRateData()
+        heartRateViewModel.generateDummyDailyHeartRateData()
+
+    }
+    private fun observeHeartRateData() {
+        heartRateViewModel.fiveMinutesHR.observe(this) { heartRate ->
+            bpmTextView.text = heartRate.avg.toString()
+        }
+    }
+
+    private fun startHeartRateUpdates() {
+        runnable = Runnable {
+            heartRateViewModel.readFromWatches(LocalDateTime.now())
+            handler.postDelayed(runnable, 5 * 60 * 1000) // 5 minutes
+        }
+        handler.post(runnable)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.connect_samsung_health, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean =
+        when (item.itemId) {
+            R.id.permission -> {
+                healthMainViewModel.connectToSamsungHealth(this)
+                true
+            }
+
+            else -> false
+        }
+
+    override fun onClick(view: View?) {
+        when (view?.id) {
+            R.id.cv_nutrition -> {
+                val permSet = mutableSetOf(
+                    Permission.of(DataTypes.NUTRITION, AccessType.READ)
+                )
+                healthMainViewModel.checkForPermission(
+                    this,
+                    permSet,
+                    AppConstants.NUTRITION_ACTIVITY
+                )
+            }
+
+            R.id.cv_step -> {
+                val permSet = mutableSetOf(
+                    Permission.of(DataTypes.STEPS, AccessType.READ)
+                )
+                healthMainViewModel.checkForPermission(this, permSet, AppConstants.STEP_ACTIVITY)
+            }
+
+            R.id.cv_heart_rate -> {
+                val permSet = mutableSetOf(
+                    Permission.of(DataTypes.HEART_RATE, AccessType.READ)
+                )
+                healthMainViewModel.checkForPermission(
+                    this,
+                    permSet,
+                    AppConstants.HEART_RATE_ACTIVITY
+                )
+            }
+
+            R.id.cv_sleep -> {
+                val permSet = mutableSetOf(
+                    Permission.of(DataTypes.SLEEP, AccessType.READ),
+                    Permission.of(DataTypes.BLOOD_OXYGEN, AccessType.READ),
+                    Permission.of(DataTypes.SKIN_TEMPERATURE, AccessType.READ)
+                )
+                healthMainViewModel.checkForPermission(this, permSet, AppConstants.SLEEP_ACTIVITY)
+            }
+        }
+    }
+
+    private fun collectResponse() {
+        lifecycleScope.launch {
+            lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
+
+                /**  Handle response of permission request */
+                launch {
+                    healthMainViewModel.permissionResponse.collect { result ->
+                        if (result.first == AppConstants.SUCCESS) {
+                            launchRespectiveActivity(result.second)
+                        } else if (result.first != AppConstants.WAITING) {
+                            showToast(this@HealthMainActivity, result.first)
+                        }
+                        healthMainViewModel.resetPermissionResponse()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun launchRespectiveActivity(activityId: Int) {
+        val intent = when (activityId) {
+            AppConstants.NUTRITION_ACTIVITY -> Intent(this, NutritionActivity::class.java)
+            AppConstants.STEP_ACTIVITY -> Intent(this, StepActivity::class.java)
+            AppConstants.HEART_RATE_ACTIVITY -> Intent(this, HeartRateActivity::class.java)
+            AppConstants.SLEEP_ACTIVITY -> Intent(this, SleepActivity::class.java)
+            else -> null
+        }
+        if (intent != null) {
+            startActivity(intent)
+        }
+    }
+}
---
+++ app/src/main/java/com/samsung/android/health/sdk/sample/healthdiary/viewmodel/HeartRateViewModel.kt
@@ -0,0 +1,186 @@
+
+package com.samsung.android.health.sdk.sample.healthdiary.viewmodel
+
+import android.app.Activity
+import androidx.databinding.ObservableField
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.samsung.android.health.sdk.sample.healthdiary.activity.HealthMainActivity
+import com.samsung.android.health.sdk.sample.healthdiary.utils.AppConstants
+import com.samsung.android.health.sdk.sample.healthdiary.utils.dateFormat
+import com.samsung.android.health.sdk.sample.healthdiary.utils.getExceptionHandler
+import com.samsung.android.sdk.healthdata.HealthConstants.*
+import com.samsung.android.sdk.health.data.HealthDataStore
+import com.samsung.android.sdk.health.data.data.HealthDataPoint
+import com.samsung.android.sdk.health.data.request.DataType
+import com.samsung.android.sdk.health.data.request.DataTypes
+import com.samsung.android.sdk.health.data.request.LocalTimeFilter
+import com.samsung.android.sdk.health.data.request.Ordering
+import kotlinx.coroutines.delay
+import java.time.LocalDateTime
+import kotlinx.coroutines.launch
+import java.util.*
+import kotlin.concurrent.timer
+import kotlin.random.Random
+
+class HeartRateViewModel(private val healthDataStore: HealthDataStore, activity: Activity) :
+    ViewModel() {
+
+    private val _exceptionResponse: MutableLiveData<String> = MutableLiveData<String>()
+    private val exceptionHandler = getExceptionHandler(activity, _exceptionResponse)
+    private val _dailyHeartRate = MutableLiveData<List<HeartRate>>()
+    private val hrResultList: MutableList<HeartRate> = mutableListOf()
+    val dailyHeartRate: LiveData<List<HeartRate>> = _dailyHeartRate
+    val dayStartTimeAsText = ObservableField<String>()
+    val exceptionResponse: LiveData<String> = _exceptionResponse
+
+    private val _fiveMinutesHR: MutableLiveData<HeartRate> = MutableLiveData<HeartRate>()
+    val fiveMinutesHR: LiveData<HeartRate> = _fiveMinutesHR
+
+    fun readHeartRateData(dateTime: LocalDateTime) {
+        dayStartTimeAsText.set(dateTime.format(dateFormat))
+
+        val localTimeFilter = LocalTimeFilter.of(dateTime, dateTime.plusDays(1))
+        val readRequest = DataTypes.HEART_RATE.readDataRequestBuilder
+            .setLocalTimeFilter(localTimeFilter)
+            .setOrdering(Ordering.DESC)
+            .build()
+
+        /**  Make SDK call to read heart rate data */
+        viewModelScope.launch(AppConstants.SCOPE_IO_DISPATCHERS + exceptionHandler) {
+            val heartRateList = healthDataStore.readData(readRequest).dataList
+            processReadDataResponse(heartRateList)
+        }
+    }
+
+    fun generateDummyDailyHeartRateData() {
+        val random = Random(System.currentTimeMillis())
+        val dummyHeartRateList = mutableListOf<HeartRate>()
+
+        // Generate dummy data for daily heart rate
+        for (i in 0 until 4) {
+            val heartRate = HeartRate(
+                min = random.nextFloat() * 40 + 60, // Random min heart rate between 60 and 100
+                max = random.nextFloat() * 40 + 100, // Random max heart rate between 100 and 140
+                avg = random.nextFloat() * 40 + 80, // Random avg heart rate between 80 and 120
+                startTime = "${i * 6}:00",
+                endTime = "${(i + 1) * 6}:00",
+                count = random.nextInt(50, 100) // Random count between 50 and 100
+            )
+            dummyHeartRateList.add(heartRate)
+        }
+
+        _dailyHeartRate.postValue(dummyHeartRateList)
+    }
+
+    fun generateDummyFiveMinutesHeartRateData() {
+        val random = Random(System.currentTimeMillis())
+
+        // Generate dummy data for five minutes heart rate
+        val fiveMinutesHeartRate = HeartRate(
+            min = random.nextFloat() * 40 + 60,
+            max = random.nextFloat() * 40 + 100,
+            avg = random.nextFloat() * 40 + 80,
+            startTime = "Last 5 Minutes",
+            endTime = "",
+            count = random.nextInt(10, 20) // Random count between 10 and 20
+        )
+
+        _fiveMinutesHR.postValue(fiveMinutesHeartRate)
+    }
+
+    fun readFromWatches(dateTime: LocalDateTime) {
+        dayStartTimeAsText.set(dateTime.format(dateFormat))
+
+        val localTimeFilter = LocalTimeFilter.of(dateTime.minusMinutes(5), dateTime)
+        val readRequest = DataTypes.HEART_RATE.readDataRequestBuilder
+            .setLocalTimeFilter(localTimeFilter)
+            .setOrdering(Ordering.DESC)
+            .build()
+
+        /** Make SDK call to read heart rate data from the past five minutes */
+        viewModelScope.launch(AppConstants.SCOPE_IO_DISPATCHERS + exceptionHandler) {
+            val heartRateList = healthDataStore.readData(readRequest).dataList
+            processReadDataResponse(heartRateList)
+        }
+    }
+
+//    private fun processReadFivePast(heartRateList: List<HealthDataPoint>) {
+//        val fiveMinutesHRData = HeartRate(1000f, 0f, 0f, "Last 5 Minutes", "", 0)
+//
+//        heartRateList.forEach { heartRateData ->
+//            processHeartRateData(heartRateData, fiveMinutesHRData)
+//        }
+//
+////        processAvgData(fiveMinutesHRData)
+////        _fiveMinutesHR.postValue(fiveMinutesHRData)
+//    }
+
+    private fun processReadDataResponse(heartRateList: List<HealthDataPoint>) {
+        hrResultList.clear()
+        val hrOfFirstQuarter = HeartRate(1000f, 0f, 0f, "00:00", "06:00", 0)
+        val hrOfSecondQuarter = HeartRate(1000f, 0f, 0f, "06:00", "12:00", 0)
+        val hrOfThirdQuarter = HeartRate(1000f, 0f, 0f, "12:00", "18:00", 0)
+        val hrOfFourthQuarter = HeartRate(1000f, 0f, 0f, "18:00", "24:00", 0)
+
+        heartRateList.forEach { heartRateData ->
+            val time = LocalDateTime.ofInstant(heartRateData.startTime, heartRateData.zoneOffset)
+            when {
+                time.isBetween(0, 5) -> processHeartRateData(heartRateData, hrOfFirstQuarter)
+                time.isBetween(6, 11) -> processHeartRateData(heartRateData, hrOfSecondQuarter)
+                time.isBetween(12, 17) -> processHeartRateData(heartRateData, hrOfThirdQuarter)
+                time.isBetween(18, 23) -> processHeartRateData(heartRateData, hrOfFourthQuarter)
+            }
+        }
+
+        processAvgData(hrOfFirstQuarter)
+        processAvgData(hrOfSecondQuarter)
+        processAvgData(hrOfThirdQuarter)
+        processAvgData(hrOfFourthQuarter)
+
+//        _dailyHeartRate.postValue(hrResultList)
+    }
+
+    data class HeartRate(
+        var min: Float,
+        var max: Float,
+        var avg: Float,
+        var startTime: String,
+        var endTime: String,
+        var count: Int
+    )
+
+    private fun processHeartRateData(heartRateData: HealthDataPoint, hrQuarter: HeartRate) {
+        hrQuarter.apply {
+            heartRateData.getValue(DataType.HeartRateType.HEART_RATE)?.let {
+                avg += it
+                count++
+            }
+            heartRateData.getValue(DataType.HeartRateType.MAX_HEART_RATE)?.let {
+                max = maxOf(max, it)
+            }
+            heartRateData.getValue(DataType.HeartRateType.MIN_HEART_RATE)?.let {
+                if (min != 0f) {
+                    min = minOf(min, it)
+                }
+            }
+            
+            
+        }
+    }
+
+    private fun processAvgData(hrQuarter: HeartRate) {
+        hrQuarter.apply {
+            if (hrQuarter.count != 0) {
+                hrQuarter.avg /= hrQuarter.count
+                hrResultList.add(hrQuarter)
+            }
+        }
+    }
+
+    private fun LocalDateTime.isBetween(fromHour: Int, toHour: Int) =
+        this >= this.withHour(fromHour).withMinute(0).withSecond(0) &&
+                this <= this.withHour(toHour).withMinute(59).withSecond(59)
+}
